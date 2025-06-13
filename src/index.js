4726bcda93933591d735570a9078accbdc758b6d
Davi-Marra <davimom1999@gmai.com>
2025-06-13 12:03:31 -0300

    finalizando desafio

---
+++ src/index.js
@@ -0,0 +1,41 @@
+let nomeHeroi = "Jorge";
+let experiencia = 7500;
+let nivel ="";
+
+if(experiencia < 1000) {
+{ nivel = ferro};
+else if (experiencia >= 1001 && <= 2000)
+{nivel = bronze};
+else if (experiencia >= 2001 && <= 5000)
+{nivel = prata};
+else if (experiencia >= 5001 && <= 7000)
+{nivel = ouro};
+else if (experiencia >= 7001 && <= 8000)
+{nivel = platininha};
+else if (experiencia >= 8001 && <= 9000)
+{nivel = ascendente};
+else if (experiencia >= 9001 && <= 10000)
+{nivel = imortal};
+else if (experiencia <= 1001)
+{nivel = radiante};
+    }
+
+    console.log(O Herói ${nomeHeroi} está no nível ${nivel}.);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
